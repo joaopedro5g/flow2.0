11a5aafcf10ce8d7720902eceb7ab4d00b3b4984
joaopedro5g <ebtjoaopedro@gmail.com>
2024-10-16 18:42:32 -0300

    LIB: Fixed memory performance for url variable

--- libflow/src/lib.rs
+++ libflow/src/lib.rs
@@ -8,14 +8,13 @@ use play::play_sound;
 #[no_mangle]
 pub extern "C" fn play(url: *const i8) -> *mut std::ffi::c_void {
   let c_str = unsafe { CStr::from_ptr(url) };
-  let url_str = c_str.to_string_lossy().to_string();
+  let url_str = c_str.to_str().unwrap();
   let url_mutex = Arc::new(Mutex::new(url_str));
   let rt = tokio::runtime::Runtime::new().expect("could not create tokio runtime");
   let url_clone = Arc::clone(&url_mutex);
   rt.spawn(async move {
     let url = url_clone.lock().await;
-    let url_str = url.as_str();
-    play_sound(&*url_str).await;
+    play_sound(&*url).await;
   });
 
   std::ptr::null_mut()
